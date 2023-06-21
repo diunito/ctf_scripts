3f0a9b6e329e31b6d735470a8dbebf6a4247d29f
alessandro <alessandromilani2000@gmail.com>
2023-06-21 18:58:03 +0200

    logging system

--- vuln/proxy_helper.py
+++ vuln/proxy_helper.py
@@ -4,6 +4,8 @@ import requests
 import os
 import glob
 
+import logging
+
 def get_container_info():
     names = {}
     client = docker.from_env()
@@ -14,7 +16,8 @@ def get_container_info():
         if ports:
             for port in ports:
                 # Stampa il nome del container e la porta mappata sull'host
-                print(f"Container: {name}, Port: {port}")
+                # print("[+] Container: " + name + " Port: " + port)
+                logging.info("[+] Container: " + name + " Port: " + port)
                 # dictionary name, port list
                 if name not in names:
                     names[name] = []
@@ -34,7 +37,8 @@ def create_json(containers):
         }            
         # if localhost:port retrun an http response add http = True
         try:
-            r = requests.get('http://localhost:' + str(containers[container]))
+            r = requests.get('http://localhost:' + str(containers[container][0]))
+            logging.info('[+] http://localhost:' + str(containers[container][0]) + ' status code:' + str(r.status_code))
             if r.status_code == 200:
                 service['http'] = True
         except:
@@ -60,12 +64,24 @@ def create_json(containers):
 
     return data  
 
+def compose_backup(compose_files):
+    # create file docker-compose.yml.bak for each docker-compose.yml on same folder
+    for compose_file in compose_files:
+        with open(compose_file, 'r') as file:
+            lines = file.readlines()
+            with open(compose_file + '.bak', 'w') as file2:
+                file2.writelines(lines)
+    # print('[+] Backup created')
+    logging.info('[+] Created backup for docker-compose.yml files')
 
 def edit_compose(main_folder, containers):
-    print()
+    logging.info('[+] Start editing docker-compose.yml files')
     compose_files = glob.glob(os.path.join(main_folder, '**/docker-compose.yml'), recursive=True) + \
                glob.glob(os.path.join(main_folder, '**/docker-compose.yaml'), recursive=True)
     compose_files = [x for x in compose_files if 'ctf_proxy' not in x]
+    
+    compose_backup(compose_files)
+    
     text_to_add = '''
 
 networks:
@@ -87,24 +103,36 @@ networks:
                         port = int(containers[conteiner][0]) + 1
                         # copy line until -
                         space = lines[i].split('-')[0]
-                        lines[i] = space + '- "' + str(port) +'"'
+                        old_line = lines[i]
+                        lines[i] = space + '- "' + str(port) +'"'+ '\n'
                         with open(compose_file, 'w') as file2:
                             file2.writelines(lines)
+                            # print folder name, old line and new line
+                            # print('[+] ' + 'Container: ' + conteiner + '\n[+] Old line: ' + lines[i] + '\n[+] New line: ' + lines[i].strip())
+                            logging.info('[+] ' + 'Container: ' + conteiner + ' Old line: ' + old_line.strip() + ' New line: ' + lines[i].strip())
                         break  
                 
-
-
     return 0
 
 if __name__ == "__main__":
-
+    
+    # start logging namee file logfile+timestamp
+    logging.basicConfig(level=logging.INFO, filename="proxy_helper.logs", filemode="a+",
+                        format="%(asctime)-15s %(levelname)-8s %(message)s")
+    logging.info("Start logging")
+    
+    # get container
     containers = get_container_info() 
+    
+    # create data strcture
     data = create_json(containers)
     json_data = json.dumps(data, indent=4)
+    
+    # write data on config.json
     with open('./ctf_proxy/proxy/config/config.json', 'w') as outfile:
         outfile.write(json_data)
-    # get curret folder
+    logging.info('[+] Updated config.json file with new containers')
+        
+    # edit docker-compose.yml files
     main_folder = os.getcwd()
-    #print(main_folder)
     res = edit_compose(main_folder, containers)
-    #print(res)
