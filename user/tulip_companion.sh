29f85c09cd3bca5b8b25242a40b40805e7f7241f
alessandro <alessandromilani2000@gmail.com>
2023-06-25 20:00:37 +0200

    Very userfulll ascii art

--- user/tulip_companion.sh
+++ user/tulip_companion.sh
@@ -2,6 +2,17 @@
 
 # get the tulip folder
 
+echo " ________  ___  ___      ___ _______           _____ ______   _______           ________  ________  ________  ________  ________  ___  ___       "
+echo "|\   ____\|\  \|\  \    /  /|\  ___ \         |\   _ \  _   \|\  ___ \         |\   __  \|\   ____\|\   __  \|\   __  \|\   ____\|\  \|\  \      "
+echo "\ \  \___|\ \  \ \  \  /  / | \   __/|        \ \  \\\__\ \  \ \   __/|        \ \  \|\  \ \  \___|\ \  \|\  \ \  \|\  \ \  \___|\ \  \ \  \     "
+echo " \ \  \  __\ \  \ \  \/  / / \ \  \_|/__       \ \  \\|__| \  \ \  \_|/__       \ \   ____\ \  \    \ \   __  \ \   ____\ \_____  \ \  \ \  \    "
+echo "  \ \  \|\  \ \  \ \    / /   \ \  \_|\ \       \ \  \    \ \  \ \  \_|\ \       \ \  \___|\ \  \____\ \  \ \  \ \  \___|\|____|\  \ \__\ \__\   "
+echo "   \ \_______\ \__\ \__/ /     \ \_______\       \ \__\    \ \__\ \_______\       \ \__\    \ \_______\ \__\ \__\ \__\     ____\_\  \|__|\|__|   "
+echo "    \|_______|\|__|\|__|/       \|_______|        \|__|     \|__|\|_______|        \|__|     \|_______|\|__|\|__|\|__|    |\_________\  ___  ___ "
+echo "                                                                                                                          \|_________| |\__\|\__\\"
+echo "                                                                                                                                       \|__|\|__|"
+echo "                                                                                                                                                 "
+echo "Start downloading pcaps from the folder /tmp/pcaps/ on the vuln box"
 if [$# -ne 3]
   then
     echo "No arguments supplied"
@@ -15,6 +26,7 @@ path=$3
 # evry 2 minutes download folder from vuln
 while true
   do
-    sshpass -p $PSWD rsync -r root@$ip:/tmp/pcap/\* $path
+    sshpass -p $PSWD rsync -vr root@$ip:/tmp/pcaps/\* $path
+    # print info about the download
     sleep 120
   done
--- vuln/dump.sh
+++ vuln/dump.sh
@@ -6,6 +6,28 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+echo " ________  ________  ________  ________  ________           ________  ___  ___  _____ ______   ________  _______   ________     "
+echo "|\   __  \|\   ____\|\   __  \|\   __  \|\   ____\         |\   ___ \|\  \|\  \|\   _ \  _   \|\   __  \|\  ___ \ |\   __  \    "
+echo "\ \  \|\  \ \  \___|\ \  \|\  \ \  \|\  \ \  \___|_        \ \  \_|\ \ \  \\\  \ \  \\\__\ \  \ \  \|\  \ \   __/|\ \  \|\  \   "
+echo " \ \   ____\ \  \    \ \   __  \ \   ____\ \_____  \        \ \  \ \\ \ \  \\\  \ \  \\|__| \  \ \   ____\ \  \_|/_\ \   _  _\  "
+echo "  \ \  \___|\ \  \____\ \  \ \  \ \  \___|\|____|\  \        \ \  \_\\ \ \  \\\  \ \  \    \ \  \ \  \___|\ \  \_|\ \ \  \\  \| "
+echo "   \ \__\    \ \_______\ \__\ \__\ \__\     ____\_\  \        \ \_______\ \_______\ \__\    \ \__\ \__\    \ \_______\ \__\\ _\ "
+echo "    \|__|     \|_______|\|__|\|__|\|__|    |\_________\        \|_______|\|_______|\|__|     \|__|\|__|     \|_______|\|__|\|__|"
+echo "                                           \|_________|                                                                         "
+echo "                                                                                                                                "
+echo "                                                                                                                                "
+
+
+# check --help
+if [ "$1" == "--help" ]; then
+  echo "Tool to automate the execution of tcpdump and optionally send them to a tulip server"
+  echo "Usage: ./start.sh <dir>  -- For execute only the tcpdump"
+  echo "Usage: ./start.sh <dir> <tulip_ip> <tulip_password> -- For execute the tcpdump and send the pcap to the tulip server"
+  echo "PS: recommended folder: /tmp/pcaps/" 
+  exit
+fi
+
+
 interrupt_handler() {
     echo "Interruzione ricevuta. Terminazione in corso..."
     # Puoi aggiungere eventuali azioni di pulizia o altre operazioni qui, se necessario.
@@ -47,7 +69,7 @@ i=1
 j=1
 # check if exist files on format CTF_dump_*.pcap
 if [ "$(ls -A $dir) | grep CTF_dump" ]; then
-    i=$(ls -A $dir | grep CTF_dump | tail -n 1 | cut -d'_' -f3 | cut -d'.' -f1)
+    i=$(ls -A $dir | grep CTF_final | tail -n 1 | cut -d'_' -f3 | cut -d'.' -f1)
     echo "Last dump file: $i"
     i=$((i+1))
     echo "Continue from $i"
@@ -57,13 +79,13 @@ fi
 while true
 do
     echo "Dumping $i"
-    timeout 120 tcpdump -i game -w ${dir}CTF_dump_$i.pcap port not 22 &
+    timeout 120 tcpdump -i game -w ${dir}CTF_final_$i.pcap port not 22 &
     tcpdump_pid=$!
     wait $tcpdump_pid
     if [ $# -eq 3 ];
       then
         echo "Uploading $i"
-        curl -F "file=@${dir}CTF_dump_$i.pcap" http://$ip:5000/upload -u "tulip:$pass"
+        curl -F "file=@${dir}CTF_final_$i.pcap" http://$ip:5000/upload -u "tulip:$pass"
     fi
     i=$((i+1))
     sleep 2
