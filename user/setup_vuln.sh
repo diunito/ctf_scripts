9b9e838e037b8d2178fc7a4a293a3843d608eaf3
koraynilay <koray.fra@gmail.com>
2024-06-17 04:20:44 +0200

    add setup_vuln.sh to copy ssh key (to clone private repo(s)) and to setup vulnbox with deploy.sh

---
+++ user/setup_vuln.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ip="$1"
+user="${2:-root}"
+port="${3:-22}"
+ssh_folder_path="../../priv/.ssh"
+key_name="vulnbox"
+home_folder=$(ssh "${user}@${ip}" 'echo $HOME')
+
+ssh -p $port "${user}@${ip}" 'mkdir -p $HOME/.ssh'
+
+rsync -Pav --rsh="ssh -p $port" "${ssh_folder_path}/${key_name}" "${ssh_folder_path}/${key_name}" "${ip}:$h$user/.ssh"
+
+ssh_conf="Host github.com
+  IdentityFile '~/.ssh/${key_name}'
+  User git"
+echo "$ssh_conf" | ssh -p $port "${user}@${ip}" 'cat - >> \$HOME/.ssh/config'
+
+ssh -p $port "${user}@${ip}" 'cd $HOME;sh <(curl -sL https://raw.githubusercontent.com/koraynilay/simad/main/deploy_clone.sh);./deploy_clone.sh'
