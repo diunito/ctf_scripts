70b0a60c9f1c45d0db83054b28d5969d562aa9e4
koraynilay <koray.fra@gmail.com>
2024-06-18 05:58:41 +0200

    Add sshpass to setup_vuln.sh

--- user/setup_vuln.sh
+++ user/setup_vuln.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
 [ -z "$1" ] && echo "need ip as first arg" && exit 1
+ssh -G '*' | grep 'controlmaster false' && echo "you should probably enable a ControlMaster in your ssh config"
 
 ip="$1"
 user="${2:-root}"
 port="${3:-22}"
 ssh_folder_path="../../priv/.ssh"
 key_name="vulnbox"
-home_folder=$(ssh "${user}@${ip}" 'echo $HOME')
 
-ssh -p $port "${user}@${ip}" 'mkdir -p $HOME/.ssh'
+sp='sshpass'
+SSHPASS=$(<../../priv/.vm-pass)
 
-rsync -Pav --rsh="ssh -p $port" "${ssh_folder_path}/${key_name}" "${ssh_folder_path}/${key_name}" "${ip}:$h$user/.ssh"
+home_folder=$($sp ssh "${user}@${ip}" 'echo $HOME')
+
+$sp ssh -p $port "${user}@${ip}" 'mkdir -p $HOME/.ssh'
+
+$sp rsync -Pav --rsh="ssh -p $port" "${ssh_folder_path}/${key_name}" "${ssh_folder_path}/${key_name}" "${ip}:$h$user/.ssh"
 
 ssh_conf="Host github.com
   IdentityFile '~/.ssh/${key_name}'
   User git"
-echo "$ssh_conf" | ssh -p $port "${user}@${ip}" 'cat - >> \$HOME/.ssh/config'
+echo "$ssh_conf" | $sp ssh -p $port "${user}@${ip}" 'cat - >> \$HOME/.ssh/config'
 
-ssh -p $port "${user}@${ip}" 'cd $HOME;sh <(curl -sL https://raw.githubusercontent.com/koraynilay/simad/main/deploy_clone.sh);./deploy_clone.sh'
+$sp ssh -p $port "${user}@${ip}" 'cd $HOME;sh <(curl -sL https://raw.githubusercontent.com/koraynilay/simad/main/deploy_clone.sh);./deploy_clone.sh'
