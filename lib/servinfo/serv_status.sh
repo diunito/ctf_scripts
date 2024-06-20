1d66ba6cd8017b2552b13020f2e2288c4968b236
koraynilay <koray.fra@gmail.com>
2024-06-21 00:09:47 +0200

    Add custom check serv (WIP)

--- lib/servinfo/serv_status.sh
+++ lib/servinfo/serv_status.sh
@@ -17,6 +17,9 @@ check_tcp() {
 	# $3: port
 	return nc -w 3 $2 $3
 }
+check_other() {
+	return $1
+}
 check_serv() {
 	# $1: service type (web, tcp)
 	# $2: ip
@@ -25,6 +28,8 @@ check_serv() {
 		return check_web $2 $3
 	elif [ "$1" = "tcp" ];then
 		return check_tcp $2 $3
+	elif [ "$1" = "other" ];then
+		return check_other $4
 	fi
 }
 
@@ -33,14 +38,15 @@ check_single_service() {
 	local ip="$2"
 	local port="$3"
 	local serv_name="$4"
-	local container_name="$4"
+	local container_name="$5"
+	local cmd="$6"
 
 	# example output:
 	# testing type web 10.60.81.1:80 fimlibrary (nginx)... OK
 	# testing type tcp 10.60.81.1:1337 rpn (rpn)... FAILED
 	# testing type sql 10.60.81.1:3306 filmlibrary (filmlibrary_sql)... FAILED
 	echo -en "testing type $serv_type\t$ip:$port $serv_name ($container_name)... "
-	if ! check_serv $serv_type $ip $port;then
+	if ! check_serv $serv_type $ip $port $cmd;then
 		echo "FAILED"
 	else
 		echo "OK"
