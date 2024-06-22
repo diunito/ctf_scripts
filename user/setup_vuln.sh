4a752b551bcefc6ff7637824f9a54dbea9d57628
koraynilay <koray.fra@gmail.com>
2024-06-22 14:53:02 +0200

    update and fixes to setup_vuln.sh

--- user/setup_vuln.sh
+++ user/setup_vuln.sh
@@ -6,21 +6,21 @@ ssh -G '*' | grep 'controlmaster false' && echo "you should probably enable a Co
 ip="$1"
 user="${2:-root}"
 port="${3:-22}"
-ssh_folder_path="../../priv/.ssh"
+ssh_folder_path="$HOME/.ssh"
 key_name="vulnbox"
 
-sp='sshpass'
-SSHPASS=$(<../../priv/.vm-pass)
+sp='sshpass -eSSHPASS'
+export SSHPASS=$(<../../priv/.vm-pass)
 
-home_folder=$($sp ssh "${user}@${ip}" 'echo $HOME')
+home_folder=$($sp ssh -p $port "${user}@${ip}" 'echo $HOME')
 
-$sp ssh -p $port "${user}@${ip}" 'mkdir -p $HOME/.ssh'
+$sp ssh -p $port "${user}@${ip}" 'mkdir -vp $HOME/.ssh'
 
-$sp rsync -Pav --rsh="ssh -p $port" "${ssh_folder_path}/${key_name}" "${ssh_folder_path}/${key_name}" "${ip}:$h$user/.ssh"
+$sp rsync -Pav --rsh="ssh -p $port" "${ssh_folder_path}/${key_name}" "${ssh_folder_path}/${key_name}.pub" "${user}@${ip}:${home_folder}/.ssh"
 
 ssh_conf="Host github.com
-  IdentityFile '~/.ssh/${key_name}'
+  IdentityFile ~/.ssh/${key_name}
   User git"
-echo "$ssh_conf" | $sp ssh -p $port "${user}@${ip}" 'cat - >> \$HOME/.ssh/config'
+echo "$ssh_conf" | $sp ssh -p $port "${user}@${ip}" 'cat - >> $HOME/.ssh/config; cat $HOME/.ssh/config'
 
-$sp ssh -p $port "${user}@${ip}" 'cd $HOME;sh <(curl -sL https://raw.githubusercontent.com/koraynilay/simad/main/deploy_clone.sh);./deploy_clone.sh'
+$sp ssh -p $port "${user}@${ip}" 'ssh-keyscan github.com >> $HOME/.ssh/known_hosts; cd $HOME;curl -sL https://raw.githubusercontent.com/koraynilay/simad/main/deploy_clone.sh -o deploy_clone.sh; chmod +x deploy_clone.sh'
