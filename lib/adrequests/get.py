51bdfd1476b047c164227441ba829aaac165b4a7
koraynilay <koray.fra@gmail.com>
2024-06-22 19:01:05 +0200

    Add adrequests lib to have custom headers for own vulnbox (works with url or ip)

---
+++ lib/adrequests/get.py
@@ -0,0 +1,31 @@
+#!/bin/false
+import requests
+import os
+
+def _new_get(url, params=None, vm_ip=None, headers=None, **kwargs):
+    r"""Sends a GET request.
+
+    :param url: URL for the new :class:`Request` object.
+    :param params: (optional) Dictionary, list of tuples or bytes to send
+        in the query string for the :class:`Request`.
+    :param \*\*kwargs: Optional arguments that ``request`` takes.
+    :param vm_ip: (optional) ip of the vulnbox to check against url
+    :return: :class:`Response <Response>` object
+    :rtype: requests.Response
+    """
+
+    if vm_ip is None:
+        vm_ip = os.getenv('VM_IP', None)
+
+    if vm_ip and vm_ip in url.split('/')[2]:
+        if headers is not None:
+            if 'skibidi' in headers.keys():
+                headers['skibidi'] = 'toilet'
+        else:
+                headers = {}
+                headers['skibidi'] = 'toilet'
+
+    return _orig_get(url, params=params, headers=headers, **kwargs)
+
+_orig_get = requests.get
+requests.get = _new_get
