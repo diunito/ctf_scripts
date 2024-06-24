a5f3e8ef7d4c5619f1d9bfdf5f053c00be59d31f
koraynilay <koray.fra@gmail.com>
2024-06-24 23:57:45 +0200

    setup_vuln.sh: fix sshpass, add gserve alias, add tmux check, add ssh-copy-id (with default key)

--- user/setup_vuln.sh
+++ user/setup_vuln.sh
@@ -10,11 +10,14 @@ ssh_folder_path="$HOME/.ssh"
 key_name="vulnbox"
 
 sp='sshpass -eSSHPASS'
-export SSHPASS=$(<../../priv/.vm-pass)
+export SSHPASS="$(<../../priv/.vm-pass)"
 
-home_folder=$($sp ssh -p $port "${user}@${ip}" 'echo $HOME')
+home_folder=$($sp ssh -p "$port" "${user}@${ip}" 'echo $HOME')
+if [ "$?" -ne 0 ];then
+	exit 1
+fi
 
-$sp ssh -p $port "${user}@${ip}" 'mkdir -vp $HOME/.ssh'
+$sp ssh -p "$port" "${user}@${ip}" 'mkdir -vp $HOME/.ssh'
 
 $sp rsync -Pav --rsh="ssh -p $port" "${ssh_folder_path}/${key_name}" "${ssh_folder_path}/${key_name}.pub" "${user}@${ip}:${home_folder}/.ssh"
 
@@ -23,4 +26,8 @@ ssh_conf="Host github.com
   User git"
 echo "$ssh_conf" | $sp ssh -p $port "${user}@${ip}" 'cat - >> $HOME/.ssh/config; cat $HOME/.ssh/config'
 
-$sp ssh -p $port "${user}@${ip}" 'ssh-keyscan github.com >> $HOME/.ssh/known_hosts; cd $HOME;curl -sL https://raw.githubusercontent.com/koraynilay/simad/main/deploy_clone.sh -o deploy_clone.sh; chmod +x deploy_clone.sh'
+$sp ssh -p "$port" "${user}@${ip}" 'echo alias gserve="''\"git init; git add -A; git -c user.name=team41 -c user.email=vulnbox@team41.ccit commit -m first\"" >> $HOME/.bash_profile'
+
+$sp ssh -p "$port" "${user}@${ip}" 'ssh-keyscan github.com >> $HOME/.ssh/known_hosts; cd $HOME;curl -sL https://raw.githubusercontent.com/koraynilay/simad/main/deploy_clone.sh -o deploy_clone.sh; chmod +x deploy_clone.sh; ./deploy_clone.sh; tmux ls'
+
+$sp ssh-copy-id -p "$port" "${user}@${ip}"
